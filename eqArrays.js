4df08587255287fb0f586bbc0a8b5dac2264ec8e
paarmin <parniaa@gmail.com>
2021-01-05 17:35:59 -0500

    done eqArr

---
+++ eqArrays.js
@@ -0,0 +1,23 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅Assertion Passed: ${actual} === ${actual}`);
+  } else {
+    console.log(`❌❌❌Assertion Failed: ${actual} !== ${actual}`);
+  }
+};
+const eqArrays = function(a, b) {
+  if (a === null || b === null) return false;
+  if (a.length !== b.length) return false;
+  for (let i = 0; i < a.length; i++) {
+    if (a[i] !== b[i]) return false;
+  }
+  return true;
+};
+
+eqArrays([1, 2, 3], [1, 2, 3]); // => true
+eqArrays([1, 2, 3], [3, 2, 1]); // => false
+
+eqArrays(["1", "2", "3"], ["1", "2", "3"]); // => true
+eqArrays(["1", "2", "3"], ["1", "2", 3]); // => false
+
+assertEqual(eqArrays([1, 2, 3], [1, 1, 3]), true); // => should PASS
