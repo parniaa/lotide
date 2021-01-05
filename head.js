51ac1169ab5c39fdeb0632d8dc9ced913c9b6d8b
paarmin <parniaa@gmail.com>
2021-01-04 20:56:58 -0500

    all done

--- head.js
+++ head.js
@@ -1,13 +1,16 @@
 // FUNCTION IMPLEMENTATION
 const assertEqual = function(actual, expected) {
-    if (actual === expected) {
-      console.log(`Assertion Passed: ${actual} === ${actual}`);
-    } else {
-      console.log(`Assertion Failed: ${actual} !== ${actual}`);
-    }
-  };
+  if (actual === expected) {
+    console.log(`✅✅✅Assertion Passed: ${actual} === ${actual}`);
+  } else {
+    console.log(`❌❌❌Assertion Failed: ${actual} !== ${actual}`);
+  }
+};
+const head = function(array) {
+  return array[0];
+};
   // TEST CODE
-  assertEqual("Lighthouse Labs", "Bootcamp");
-  assertEqual("Lighthouse Labs", "Lighthouse Labs");
-  assertEqual(2.2, 2);
-  assertEqual(1, 1);
+assertEqual(head([5,6,7]), 5);
+assertEqual(head([5]), 5);
+assertEqual(head([]),);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
--- tail.js
+++ tail.js
@@ -1,13 +1,19 @@
 // FUNCTION IMPLEMENTATION
 const assertEqual = function(actual, expected) {
-    if (actual === expected) {
-      console.log(`Assertion Passed: ${actual} === ${actual}`);
-    } else {
-      console.log(`Assertion Failed: ${actual} !== ${actual}`);
+  if (actual === expected) {
+    console.log(`✅✅✅Assertion Passed: ${actual} === ${actual}`);
+  } else {
+    console.log(`❌❌❌Assertion Failed: ${actual} !== ${actual}`);
+  }
+};
+  const tail = function(array) {
+  let newArray = [];
+  for (let i = 1; i < array.length; i++) {
+      newArray[i] = array[i];
     }
+    return newArray;
   };
   // TEST CODE
-  assertEqual("Lighthouse Labs", "Bootcamp");
-  assertEqual("Lighthouse Labs", "Lighthouse Labs");
-  assertEqual(2.2, 2);
-  assertEqual(1, 1);
+const words = ["Yo Yo", "Lighthouse", "Labs"];
+tail(words); // no need to capture the return value since we are not checking it
+assertEqual(words.length, 3); 
