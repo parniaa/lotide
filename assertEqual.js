151498071d21120be574a10a3d96c4d50af8e6b0
paarmin <parniaa@gmail.com>
2021-01-04 19:05:24 -0500

    kk

--- assertEqual.js
+++ assertEqual.js
@@ -1,12 +1,13 @@
 // FUNCTION IMPLEMENTATION
 const assertEqual = function(actual, expected) {
-    if (actual === expected) {
-        console.log("Assertion Passed:" + actual +  " === " + expected);
-    } else {
-        console.log("Assertion Failed:" + actual +  " !== " + expected);
-    }
+  if (actual === expected) {
+    console.log("Assertion Passed:" + actual +  " === " + expected);
+  } else {
+    console.log("Assertion Failed:" + actual +  " !== " + expected);
+  }
 };
-
 // TEST CODE
 assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual("Lighthouse Labs", "Lighthouse Labs");
+assertEqual(2.2, 2);
 assertEqual(1, 1);
