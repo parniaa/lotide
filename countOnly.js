8f205354c224001a7bf9146ab799f0f3a57c7dfa
paarmin <parniaa@gmail.com>
2021-01-06 14:26:09 -0500

    added

--- countOnly.js
+++ countOnly.js
@@ -9,6 +9,19 @@ const countOnly = function(allItems, itemsToCount) {
   const results = {};
   for (const item of allItems) {
     console.log(item);
+    
+    if (results[item]) {
+      results[item] += 1;
+    } else {
+      results[item] = 1;
+    }
+    if (itemsToCount[item]) { 
+      if (results[item]) {
+        results[item] += 1;
+      } else {
+        results[item] = 1;
+      }
+    }
   }
 
   return results;
@@ -27,7 +40,12 @@ const firstNames = [
   "Joe"
 ];
 
-const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true, "Agouhanna": false });
+const result1 = countOnly(firstNames, {
+  "Jason": true,
+  "Karima": true,
+  "Fang": true,
+  "Agouhanna": false
+});
 
 assertEqual(result1["Jason"], 1);
 assertEqual(result1["Karima"], undefined);
