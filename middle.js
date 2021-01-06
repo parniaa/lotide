b68d99ddf2586005403d47716e4b5fb2f24d9f75
paarmin <parniaa@gmail.com>
2021-01-06 09:30:23 -0500

    middle

---
+++ middle.js
@@ -0,0 +1,20 @@
+const eqArrays = function(a, b) {
+  if (a === null || b === null) return false;
+  if (a.length !== b.length) return false;
+  for (let i = 0; i < a.length; i++) {
+    if (a[i] !== b[i]) return false;
+  }
+  return true;
+};
+const assertArraysEqual = function(actual, expected) {
+  if (eqArrays(actual,expected)) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${actual}`);
+  } else {
+    console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+const middle = function(array) {
+
+};
+middle([1]) // => []
+middle([1, 2])
