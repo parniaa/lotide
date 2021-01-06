dbc6864fb3bbceb47604355f2b441c1cc6198ccd
paarmin <parniaa@gmail.com>
2021-01-05 20:31:54 -0500

    final

--- without.js
+++ without.js
@@ -26,10 +26,10 @@ const without = function(source, itemsToRemove) {
 
 // without([1, 2, 3], [1]) // => [2, 3]
 // without(["1", "2", "3"], [1, 2, "3"]) // => ["1", "2"]
-// console.log(without([1, 2, 3], ["1"]));
+console.log(without([1, 2, 3], ["1"]));
 
-const words = ["hello", "world", "lighthouse"];
-without(words, ["lighthouse"]); // no need to capture return value for this test case
-// Make sure the original array was not altered by the without function
-assertArraysEqual(words, ["hello", "world", "lighthouse"]);
+// const words = ["hello", "world", "lighthouse"];
+// without(words, ["lighthouse"]); // no need to capture return value for this test case
+// // Make sure the original array was not altered by the without function
+// assertArraysEqual(words, ["hello", "world", "lighthouse"]);
 
