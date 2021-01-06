b878018654da4d32058d34173e1a0286234674f4
paarmin <parniaa@gmail.com>
2021-01-05 20:29:03 -0500

    kk

--- without.js
+++ without.js
@@ -14,7 +14,22 @@ const assertArraysEqual = function(actual, expected) {
   }
 };
 const without = function(source, itemsToRemove) {
-
+  if (eqArrays(source, itemsToRemove)) console.log([]);
+  let resultArray = [];
+  for (let i = 0; i < source.length; i++) {
+    if (!itemsToRemove.includes(source[i])) {
+      resultArray.push(source[i]);
+    }
+  }
+  return resultArray;
 };
 
-assertArraysEqual([1, 2, 3], [1, 2, 4]);
+// without([1, 2, 3], [1]) // => [2, 3]
+// without(["1", "2", "3"], [1, 2, "3"]) // => ["1", "2"]
+// console.log(without([1, 2, 3], ["1"]));
+
+const words = ["hello", "world", "lighthouse"];
+without(words, ["lighthouse"]); // no need to capture return value for this test case
+// Make sure the original array was not altered by the without function
+assertArraysEqual(words, ["hello", "world", "lighthouse"]);
+
