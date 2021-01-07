94aa03df11199c8a2f1fbf09006f9be08c0c1bd5
paarmin <parniaa@gmail.com>
2021-01-07 15:31:53 -0500

    mapjs test cases added

--- map.js
+++ map.js
@@ -19,13 +19,18 @@ const map = function(array, callback) {
     // console.log('item BEFORE: ', item);
     // console.log('item AFTER: ', callback(item));
     // console.log('---');
-    results.push(callback(item));
+    if (item === "") {
+      results.push("")
+    } else {
+      results.push(callback(item));
+    }
   }
   return results;
-}
+};
 
 const words = ["ground", "control", "to", "major", "tom"];
-const results1 = map(words, word => word[0]);
+const word1 = ["", "c", "to", "mar", "pom"];
+const results1 = map(word1, word => word[0]);
 console.log(results1);
 assertArraysEqual(map(words, word => word[0]), [ 'g', 'c', 't', 'm', 't' ]);
-
+assertArraysEqual(map(word1, word => word[0]), [ '', 'c', 't', 'm', 'p' ]);
