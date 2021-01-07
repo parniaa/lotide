a0dfb2992cbee71ba85e887d4c7f8fdb64cfb3e1
paarmin <parniaa@gmail.com>
2021-01-07 15:17:03 -0500

    kk map

---
+++ map.js
@@ -0,0 +1,31 @@
+const eqArrays = function(a, b) {
+  if (a === null || b === null) return false;
+  if (a.length !== b.length) return false;
+  for (let i = 0; i < a.length; i++) {
+    if (a[i] !== b[i]) return false;
+  }
+  return true;
+};
+const assertArraysEqual = function(actual, expected) {
+  if (eqArrays(actual,expected)) {
+    console.log(`✅✅✅Assertion Passed: ${actual} === ${actual}`);
+  } else {
+    console.log(`❌❌❌Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+const map = function(array, callback) {
+  const results = [];
+  for (let item of array) {
+    // console.log('item BEFORE: ', item);
+    // console.log('item AFTER: ', callback(item));
+    // console.log('---');
+    results.push(callback(item));
+  }
+  return results;
+}
+
+const words = ["ground", "control", "to", "major", "tom"];
+const results1 = map(words, word => word[0]);
+console.log(results1);
+assertArraysEqual(map(words, word => word[0]), [ 'g', 'c', 't', 'm', 't' ]);
+
