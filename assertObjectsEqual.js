1c7a9224dc74cbb25d45c3a9e0e34c98316eb498
paarmin <parniaa@gmail.com>
2021-01-07 09:03:32 -0500

    actual implemented in assertObjectEqual

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -34,5 +34,18 @@ const eqArrays = function(a, b) {
 
 // FUNCTION IMPLEMENTATION
 const assertObjectsEqual = function(actual, expected) {
+  const inspect = require('util').inspect;
+  // console.log(`Example label: ${inspect(actual)}`);
   // Implement me!
+  if (eqObjects(actual,expected)) {
+    console.log(`✅✅✅Assertion Passed: ${inspect(actual)} === ${inspect(actual)}`);
+  } else {
+    console.log(`❌❌❌Assertion Failed: ${inspect(actual)} !== ${inspect(expected)}`);
+  }
 };
+const cd = { c: "1", d: ["2", 3] };
+const dc = { d: ["2", 3], c: "1" };
+const cd2 = { c: "1", d: ["2", 3, 4] };
+
+assertObjectsEqual(cd, dc);
+assertObjectsEqual(cd, cd2);
