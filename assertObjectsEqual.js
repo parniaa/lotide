5135990ad6b6e43719c146e9c1c07daa1d8d88fb
paarmin <parniaa@gmail.com>
2021-01-07 09:16:04 -0500

    final cmmt

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -11,11 +11,8 @@ const eqObjects = function(object1, object2) {
   let keys1 = Object.keys(object1);
   let keys2 = Object.keys(object2);
   if (keys1.length !== keys2.length) return false;
-  // for (let i = 0; i < keys1.length; i++) {
-  //   if (keys1[i] !== keys2[i]) return false;
-  // }
   for (const key in object1) {
-    if (!object2.hasOwnProperty(key)) return  false;
+    if (!Object.prototype.hasOwnProperty.call(object2, key)) return  false;
     if (!Array.isArray(object1[key]) && !Array.isArray(object2[key])) {
       if (object1[key] !== object2[key]) return false;
     }
