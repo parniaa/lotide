4dde9155ae18149dceee6fbd787a7863494735d6
paarmin <parniaa@gmail.com>
2021-01-07 09:10:42 -0500

    eslint fix

--- eqObjects.js
+++ eqObjects.js
@@ -5,27 +5,7 @@ const assertEqual = function(actual, expected) {
     console.log(`❌❌❌Assertion Failed: ${actual} !== ${expected}`);
   }
 };
-// Returns true if both objects have identical keys with identical values.
-// Otherwise you get back a big fat false!
-const eqObjects = function(object1, object2) {
-  let keys1 = Object.keys(object1);
-  let keys2 = Object.keys(object2);
-  if (keys1.length !== keys2.length) return false;
-  // for (let i = 0; i < keys1.length; i++) {
-  //   if (keys1[i] !== keys2[i]) return false;
-  // }
-  for (const key in object1) {
-    // if (!Object.prototype.hasOwnProperty(object2, key)) return  false;
-    if (!object2.hasOwnProperty(key)) return  false;
-    if (!Array.isArray(object1[key]) && !Array.isArray(object2[key])) {
-      if (object1[key] !== object2[key]) return false;
-    }
-    if (!eqArrays(object1[key], object2[key])) return false;
-  }
 
-  
-  return true;
-};
 const eqArrays = function(a, b) {
   if (a === null || b === null) return false;
   if (a.length !== b.length) return false;
@@ -35,16 +15,26 @@ const eqArrays = function(a, b) {
   return true;
 };
 
-//Another Solution=====>
-// const findKeyByValue = function(inputObject, inputValue) {
-//   let objectKeys = Object.keys(inputObject);
-//   for (const objectK of objectKeys) {
-//     console.log(inputObject[objectK], inputValue);
-//     if (inputObject[objectK] === inputValue) {
-//       return objectK;
-//     }
-//   }
-// };
+// Returns true if both objects have identical keys with identical values.
+// Otherwise you get back a big fat false!
+
+const eqObjects = function(object1, object2) {
+  let keys1 = Object.keys(object1);
+  let keys2 = Object.keys(object2);
+  if (keys1.length !== keys2.length) return false;
+  for (const key in object1) {
+    // if (!object2.hasOwnProperty(key)) return  false; <=== lint error
+    if (!Object.prototype.hasOwnProperty.call(object2, key)) return  false;
+    if (!Array.isArray(object1[key]) && !Array.isArray(object2[key])) {
+      if (object1[key] !== object2[key]) return false;
+    }
+    if (!eqArrays(object1[key], object2[key])) return false;
+  }
+
+  
+  return true;
+};
+
 
 // const ab = { a: "1", b: "2" };
 // const ba = { b: "2", a: "1" };
