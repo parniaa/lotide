b3edcec93edf116bad19aadec6acec8820189e15
paarmin <parniaa@gmail.com>
2021-01-11 13:41:09 -0500

    tail Mocha Chai

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,7 +1,22 @@
 const tail = require("../tail");
-const assertEqual = require("../assertEqual");
-
+const assert = require("chai").assert;
 // TEST CODE
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-// tail(words); // no need to capture the return value since we are not checking it
-assertEqual(tail(words).length, 3);
+// const words = ["Yo Yo", "Lighthouse", "Labs"];
+// // tail(words); // no need to capture the return value since we are not checking it
+// assertEqual(tail(words).length, 3);
+
+
+describe("#head", () => {
+  it("returns length of 3 for [1, 2, 3]", () => {
+    assert.deepEqual(tail([1, 2, 3]).length, 3);
+  });
+  it("returns length of 0 for []", () => {
+    assert.deepEqual(tail([]).length, 0);
+  });
+  it(`returns length of 1 for ["A"]`, () => {
+    assert.deepEqual(tail(["A"]).length, 0);
+  });
+  // it(`returns 'Hello' for ["Hello", "Lighthouse", "Labs\"]`, () => {
+  //   assert.strictEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+  // });
+});
